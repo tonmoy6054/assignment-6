5ab6ee4a9ae7c8b0df6a770a0f6cb2e20bd11805
Tonmoy Ghosh <tonmoy.mec@gmail.com>
2023-03-16 15:16:18 +0600

    commenting in js

--- index1.js
+++ index1.js
@@ -1,3 +1,4 @@
+// fetching the data
 const loadAllData = async() => {
   const URL = `https://openapi.programming-hero.com/api/ai/tools`;
   // console.log(URL);
@@ -6,6 +7,7 @@ const loadAllData = async() => {
     showAllData(data.data);
     
     };
+    // displaying the data
     const showAllData = (data) => {
         const mideaContainer = document.getElementById("midea-container");
         // console.log(data);
@@ -39,7 +41,7 @@ const loadAllData = async() => {
       };
       
     loadAllData();
-
+// show all button add
     document.getElementById("btnShowAll").addEventListener('click', function(){
      
   
@@ -85,7 +87,7 @@ const loadAllData = async() => {
     
     showFinalData();
   });
-  
+  // fetching each card details data
   const loadSingleData = async id => {
     
     const URL = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
@@ -95,17 +97,10 @@ const loadAllData = async() => {
       displaySingleDataDetails(data);
       
       };
+// displaying each card detils data in modal
 
-     
-      
-    
-
-      function displaySingleDataDetails(data) {
-console.log(data.data.features);
-
-
-
-
+    function displaySingleDataDetails(data) {
+// console.log(data.data.features);
 const modalDescription = document.getElementById("modal-description");
 modalDescription.innerText = data.data.description;
 const modalAccuracy = document.getElementById('accuracy-rate');
@@ -123,108 +118,30 @@ for(const user of data.data.pricing){
   modalPricing.appendChild(li);
 
 }
-const modalFeatures = document.getElementById('modal-features');
-modalFeatures.innerHTML = `
-<p><b>Featuress</b>: ${data.data.features.feature_name}</p>
+
+const modalImg = document.getElementById('modal-img');
+for(const user of data.data.image_link){
+  // console.log(user);
+  const img = document.createElement('img');
+  // img.innerText = user;
+img.innerHTML = `
+<figure class="px-10 pt-10">
+    <img src="${data.data.image_link}" alt="chatGPT" class="rounded-xl"/>
+   </figure>
 `;
-// for (const user of data.features){
-//   console.log(user);
-//   const li = document.createElement('li');
-//   li.innerText = data.data.features.feature_name;
-//   modalFeatures.appendChild(li);
-
-// modalFeatures.innerText = data.data.features.feature_name;
-// }
-
-// const modalImg = document.getElementById('modal-img');
-// for(const user of data.data.image_link){
-//   console.log(user);
-//   const img = document.createElement('img');
-//   img.innerText = user;
   
-//   modalImg.appendChild(img);
-  
- 
-// }
+  modalImg.appendChild(img);
+
+}
 
 
 const featuresIntegrations = document.getElementById("features & integrations");
 featuresIntegrations.innerHTML = `
  <p><b>Integrations</b>: ${data.data.integrations}</p>
 `;
-// const modalPricing = document.getElementById("modal-pricing");
-// modalPricing.innerText = toStr.plan;
-
-// console.log(data.features.feature_name);
 
 };
 
 loadSingleData();
 
-// let dev = [
-//   {plan: 'Basic', price: '$10/month'},
-  
-//   {plan: 'Pro', price: '$50/month'},
-  
-//   {plan: 'Enterprise', price: 'Contact us for pricing'},
-//   ];
-//   let toStr = dev.map(function(loadSingleData){
-//   return `${loadSingleData.plan} ${loadSingleData.price}`;
-//   });
-//   toStr = toStr.join(", ")
-  // console.log(toStr);
-  // console.log(typeof toStr);
 
-
-
-
-
-
-//         data.data.tools.slice(0, 6).forEach(element => {
-//           // console.log(element.published_in.sort(customSort));
-//         //    console.log(data.data.tools[0].image);
-//         // console.log(data.data.tools);
-//         // console.log(element.published_in);
-//         const container = document.getElementById("section");
-       
-// const div = document.createElement('div');
-// div.innerHTML = `
-// <div class="card w-96 bg-base-100 shadow-2xl">
-//   <figure class="px-10 pt-10">
-//     <img src="${element.image}" alt="chatGPT" class="rounded-xl"/>
-//   </figure>
-//   <div class="card-body items-center text-center">
-//     <h2 class="card-title">Features</h2>
-//     <p><ol>
-//     <li>1.Natural language processing</li>
-//     <li>2.Contextual understanding</li>
-//     <li>3.Text generation</li>
-//     </ol>
-//     <h2 class="card-title">${element.name}</h2>
-//     <p class="sort-by-date">${element.published_in}</p>
-//     <button onclick="singleDataDetailsModal('${element.id}')"class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#singleDataDetailsModal">Details</button>
-    
-      
-//     </div>
-//   </div>
-// </div>
-// `;
-// container.appendChild(div);
-
-// }); 
-
-//     };
-//   const singleDataDetailsModal = (id) => {
-// // console.log(id);
-// const URL = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
-
-// fetch(URL).then((res) => res.json()).then((data) => singleDataDetailsModal(data));
-
-//   };
-// "use strict";
-// let pricing =[];
-// fetch('https://openapi.programming-hero.com/api/ai/tool/${id}')
-// .then(response => response.json())
-// .then(json => pricing = json.map(obj => obj.pricing));
-
- 
